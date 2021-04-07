3d880d48cd9e5292523907dff0953accd6430469
Kaj-Michael Lang <milang@tal.org>
2021-04-07 09:40:09 +0300

    Remove locations with no street name and also special postal codes

--- create-nominatim-postcodes.sql
+++ create-nominatim-postcodes.sql
@@ -1,15 +1,11 @@
 --
 -- Create table with postcode center, radius and the concave area of the postal code points
+-- We skip importing of special postal codes (PO boxes, etc) and locations with no street name.
 --
 
 CREATE TABLE fi_postcode_areas AS SELECT ST_ConcaveHull(ST_Collect(wkb_geometry), 0.99) AS area,
  ST_Centroid(ST_Collect(ST_Points(wkb_geometry))) as center,
  (ST_MinimumBoundingRadius(ST_Collect(wkb_geometry))).radius as radius, postal_code, region, municipality
- FROM finland_addresses WHERE street is not null
+ FROM finland_addresses WHERE street != '' AND postal_code not in ('00000','00002','00099')
  GROUP by postal_code,region,municipality;
 
---
--- Remove 00000 code
---
-
-DELETE FROM fi_postcode_areas WHERE postal_code='00000';
